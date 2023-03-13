86becf094ce2c33de8e6c3ef0fd6710dae932ffc
tominaduris <tominaduris@hotmail.com>
2023-03-13 13:44:21 -0300

    correccion del metodo crear persona

--- src/main/java/com/portfolio/nt/Controller/PersonaController.java
+++ src/main/java/com/portfolio/nt/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,12 @@ public class PersonaController {
         
         Persona persona = personaService.getOne(id).get();
         return new ResponseEntity(persona, HttpStatus.OK);
+    }
+    
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona){
+        personaService.save(persona);
+        return "La persona fue creada correctamente";
     }
     
     /*@DeleteMapping("/delete/{id}")
